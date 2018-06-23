b771927940b5b0dc9edbbc3d4b63da5ac355153e
maxoyed <maxoyed@gmail.com>
2018-06-23 17:17:31 +0800

    修复无法导入的问题

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cquauth',
-    version = '0.0.1',
+    version = '0.0.3',
     keywords='spider cqu oauth',
     description = ('重庆大学统一认证'),
     long_description=open('README.rst').read(),
